75d1ec85ae111ae21471beb68203d22531136e12
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-12-05 16:14:09 +0100

    Adding new customer

--- src/main/java/com/luv2code/springdemo/controller/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerRestController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springdemo.entity.Customer;
-import com.luv2code.springdemo.error.CustomerErrorResponse;
 import com.luv2code.springdemo.error.CustomerNotFoundException;
 import com.luv2code.springdemo.service.CustomerService;
 
@@ -31,6 +32,14 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer with id: " + id + " not found");
 		}
 		return customerService.getCustomer(id);
+	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		// if id is equal to 0 DAO will INSERT new customer
+		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
 	}
 
 }
