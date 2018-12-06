537147667b450f3d1df80335c2d17e321a35b8ed
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-12-06 12:51:37 +0100

    Adding @PutMapping and @DeleteMapping

--- src/main/java/com/luv2code/springdemo/controller/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,21 @@ public class CustomerRestController {
 		customer.setId(0);
 		customerService.saveCustomer(customer);
 		return customer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+	
+	@DeleteMapping("/customers/{id}")
+	public String deleteCustomer(@PathVariable int id) {
+		if(customerService.getCustomer(id) == null) {
+			throw new CustomerNotFoundException("Customer id not found: " + id);
+		}
+		customerService.deleteCustomer(id);
+		return "Customer with id: " + id + " was deleted.";
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,7 +6,8 @@
 <hr>
 <a href="${pageContext.request.contextPath}/api/customers">Get all customers</a> <br>
 <a href="${pageContext.request.contextPath}/api/customers/1">Get first customer</a> <br>
-<a href="${pageContext.request.contextPath}/api/customers/2">Get second customer</a> <br>
+<a href="${pageContext.request.contextPath}/api/customers/2">Get second customer</a> <hr>
+
 
 </body>
 </html>
